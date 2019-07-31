bf96b470709725128758f35b34082de34fe79276
Roc-zhou <13031902163@163.com>
2019-07-31 11:01:40 +0800

    添加统计

--- src/router/index.js
+++ src/router/index.js
@@ -7,4 +7,9 @@ Vue.use(Router)
 export default new Router({
   mode: 'history',
   routes: components,
+  scrollBehavior(to, from, savedPosition) { 
+    if (to.path) { // 解决vue单页面路由不能触发百度统计
+      window._hmt.push(['_trackPageview', to.fullPath]);
+    }
+  },
 })
